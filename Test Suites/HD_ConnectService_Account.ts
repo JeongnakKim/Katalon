c345ffc59a612fbf13ff49c8d26b8f6fd26e4816
hancom <hancom@DESKTOP-PTKT1R2>
2023-05-10 15:15:25 +0900

    remove TestCases-Recorders
    create
    TestSuites- HD_ConnectService_Account, HD_ConnectService_Billing

---
+++ Test Suites/HD_ConnectService_Account.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>HD_ConnectService_Account</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>5363f010-afe3-4c81-85de-c703186da017</testSuiteGuid>
+</TestSuiteEntity>
---
+++ Test Suites/HD_ConnectService_Billing.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>HD_ConnectService_Billing</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>8927e993-0f8a-4ebe-bd8b-d5ca3433a979</testSuiteGuid>
+</TestSuiteEntity>
